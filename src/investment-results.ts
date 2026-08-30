6d73d1c1628bb83b9f56d734338e31ad89b5ade5
allaman Konate <113556544+konateallaman@users.noreply.github.com>
2026-08-30 16:55:26 -0500

    Rename _calculateInvestmentResults to calculateInvestmentResults

--- src/investment-results.ts
+++ src/investment-results.ts
@@ -2,7 +2,7 @@
 // e.g., integrate it into a service or component
 // You may need to tweak it, depending on where and how you use it
 
-function _calculateInvestmentResults() {
+function calculateInvestmentResults() {
   const annualData = [];
   let investmentValue = initialInvestment;
 
